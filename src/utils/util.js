58a978788b47d4a8b3451fd5904132fcb9ea55ae
yanmengs <1657294032@qq.com>
2025-02-21 19:06:59 +0800

    fix: 更换url

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,6 +1,6 @@
 //utils.js
 import axios from "axios";
-axios.defaults.baseURL = "http://e7f60b4.r20.cpolar.top";
+axios.defaults.baseURL = "http://4e4e747a.r11.cpolar.top";
 const axiosInstance = axios.create({
   timeout: 5 * 1000, // 请求超时时间（5秒）
   retry: 0, //全局重试次数
